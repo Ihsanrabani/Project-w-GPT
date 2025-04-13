3f1dd460ffcd540e18654f7abbb8a5fdfa0c3054
Ihsanrabani <ztex2727@gmail.com>
2025-04-13 17:51:56 +0700

    Small Fixing/Trying

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { useState, useEffect, use} from 'react'
+import { useState, useEffect} from 'react'
 
 function App() {
 
@@ -7,10 +7,8 @@ function App() {
   // 1. Count si habit nya
   // 2. Setiap hari si habit completed dan tombol done nya tereset [📝TEST BESOK]
 
-
   // Mengambil Data dari LocalStorage
   useEffect(() => {
-    localStorage.setItem("LastOpenedDate", currentDate)
     const savedHabits = JSON.parse(localStorage.getItem("Habits")) || []
     const lastDate = localStorage.getItem("LastOpenedDate")
     
@@ -42,6 +40,7 @@ function App() {
   const year = date.getFullYear()
 
   const currentDate = day + '-' + month + '-' +year
+  const currentDatee = day +1 + '-' + month + '-' +year
 
   const handleAddHabit = (e) => {
     e.preventDefault();
@@ -95,115 +94,120 @@ function App() {
     }
   }, [habits])
 
+  // ⚠️⚠️ MASIH ADA KEMUNGKINAN ERROR YANG FITUR HABIT RESET SETIAP HARI⚠️⚠️
+
   return (
     <>
-      <nav className='py-4 text-center bg-gray-200 mb-10'>
-        <h1 className='text-xl font-bold hover:text-green-400 ease-out duration-300'>Habit Tracker Harian</h1>
-      </nav>
+      <div className='min-h-screen flex flex-col'>
+        <nav className='py-4 text-center bg-gray-200 mb-10'>
+          <h1 className='text-xl font-bold hover:text-green-400 ease-out duration-300'>Habit Tracker Harian</h1>
+        </nav>
 
-      <div className='px-2 md:px-24 xl:px-[30%]'>
-        
-        <h1 className='text-xl mb-10 font-bold'>Tanggal: {currentDate}</h1>
+        <div className='px-2 md:px-24 xl:px-[30%]'>
 
-        {/* Habits List Section */}
-        <div>
+          <h1 className='text-xl mb-10 font-bold'>Tanggal: {currentDate}</h1>
+          <button onClick={() => { sim1Day() }}>Simulasi 1 Hari</button>
 
-          {/* HABITS LIST/CARDS */}
-          <div className='mb-10'>
-            <div className='border border-b-0 border-slate-300 mx-[30%] rounded-t-md'>
-              <h1 className='text-center text-xl font-bold'>Habits Kamu!</h1>
-            </div>
+          {/* Habits List Section */}
+          <div>
 
-            <div className='border border-slate-300 mx-2 rounded-md mb-3'>
-              {
-                isAny &&
-                <h1 className='text-xl text-center py-10 font-bold text-gray-400'>Kamu belum punya habit!</h1>
-              }
-              {habits.map((habit, index) => (
-                <div className='bg-slate-200 m-3 rounded-md px-2 py-1 relative' key={index}>
-                  <div className=''>
-                    <div className='flex justify-between items-center'>
-                      <h1 className='text-xl font-bold '>{habit.Title}</h1>
-                      <p className='text-sm text-gray-400/70'>Created: {habit.CreatedDate}</p>
+            {/* HABITS LIST/CARDS */}
+            <div className='mb-10'>
+              <div className='border border-b-0 border-slate-300 mx-[30%] rounded-t-md'>
+                <h1 className='text-center text-xl font-bold'>Habits Kamu!</h1>
+              </div>
+
+              <div className='border border-slate-300 mx-2 rounded-md mb-3'>
+                {
+                  isAny &&
+                  <h1 className='text-xl text-center py-10 font-bold text-gray-400'>Kamu belum punya habit!</h1>
+                }
+                {habits.map((habit, index) => (
+                  <div className='bg-slate-200 m-3 rounded-md px-2 py-1 relative' key={index}>
+                    <div className=''>
+                      <div className='flex justify-between items-center'>
+                        <h1 className='text-xl font-bold '>{habit.Title}</h1>
+                        <p className='text-sm text-gray-400/70'>Created: {habit.CreatedDate}</p>
+                      </div>
+                      <p className='text-sm text-ellipsis'>{habit.Desc}</p>
                     </div>
-                    <p className='text-sm text-ellipsis'>{habit.Desc}</p>
+                    <div className='flex gap-2 justify-end'>
+                      <button className={`p-1 px-3 rounded-md shadow-md hover:-translate-y-1 hover:shadow-lg hover:bg-green-450 ease-out duration-300 ${habit.Completed ? `bg-gray-400 text-white cursor-not-allowed` : `bg-green-400 text-gray-800`}`} onClick={() => handleHabitDone(habit, index)}>Done</button>
+                      <button className={`p-1 px-3 rounded-md shadow-md hover:-translate-y-1 hover:shadow-lg hover:bg-green-450 ease-out duration-300 bg-red-400 text-white`} onClick={() => { handleDeleteHabit(index, habit) }}>Delete</button>
+                    </div>
                   </div>
-                  <div className='flex gap-2 justify-end'>
-                    <button className={`p-1 px-3 rounded-md shadow-md hover:-translate-y-1 hover:shadow-lg hover:bg-green-450 ease-out duration-300 ${habit.Completed ? `bg-gray-400 text-white cursor-not-allowed` : `bg-green-400 text-gray-800`}`} onClick={() => handleHabitDone(habit, index)}>Done</button>
-                    <button className={`p-1 px-3 rounded-md shadow-md hover:-translate-y-1 hover:shadow-lg hover:bg-green-450 ease-out duration-300 bg-red-400 text-white`} onClick={() => { handleDeleteHabit(index, habit) }}>Delete</button>
-                  </div>
+                ))}
+              </div>
+              <div className='mx-2 text-right'>
+                <button className='bg-sky-300 text-gray-800 py-1 px-2 rounded-md' onClick={() => { setIsVisible(true) }}>
+                  Tambah Habit
+                </button>
+              </div>
+            </div>
+          </div>
+
+
+
+          {/* PENCAPAIAN HABITS */}
+          <div className='border border-b-0 border-slate-300 mx-[30%] rounded-t-md'>
+            <h1 className='text-center text-xl font-bold'>Pencapaian Habits Kamu</h1>
+          </div>
+          <div>
+            <div className='border border-slate-300 rounded-md flex flex-col gap-5 mx-2 p-3 md:grid md:grid-cols-2'>
+
+              {habits.map((habit, index) => (
+                <div className='bg-slate-200 text-center py-4 mx-[15%] ' key={index}>
+                  <h1 className='text-3xl font-bold mb-4'>{habit.Title}</h1>
+                  <p className='text-md mb-3'>Completed:</p>
+                  <h1 className='text-4xl font-bold'>{habit.CompletedCount}</h1>
                 </div>
               ))}
-            </div>
-            <div className='mx-2 text-right'>
-              <button className='bg-sky-300 text-gray-800 py-1 px-2 rounded-md' onClick={() => { setIsVisible(true) }}>
-                Tambah Habit
-              </button>
+
             </div>
           </div>
-          </div>
 
-
-
-        {/* PENCAPAIAN HABITS */}
-        <div className='border border-b-0 border-slate-300 mx-[30%] rounded-t-md'>
-          <h1 className='text-center text-xl font-bold'>Pencapaian Habits Kamu</h1>
-        </div>    
-        <div>
-          <div className='border border-slate-300 rounded-md flex flex-col gap-5 mx-2 p-3 md:grid md:grid-cols-2'>
-
-            {habits.map((habit, index) => (
-              <div className='bg-slate-200 text-center py-4 mx-[15%] '>
-                <h1 className='text-3xl font-bold mb-4'>{habit.Title}</h1>
-                <p className='text-md mb-3'>Completed:</p>
-                <h1 className='text-4xl font-bold'>{habit.CompletedCount}</h1>
-              </div>
-            ))}
-
-          </div>
         </div>
 
+
+        {/* Pop-up */}
+        {
+          isVisible &&
+          <div className={`absolute h-screen w-screen inset-0 bg-gray-500/40 flex flex-col justify-center ease-in duration-300`}>
+            <div className='bg-white mx-[10%] md:mx-[30%] xl:mx-[34%] p-4 rounded-md'>
+              <div className='flex justify-between mb-4'>
+                <h1 className='text-xl font-bold'>Tambah Habit</h1>
+                <button className='border border-black/50 w-7 h-7 rounded-full' onClick={() => { setIsVisible(false) }}>X</button>
+              </div>
+              <form action="" className='text-center flex flex-col px-8 gap-3'>
+                <input
+                  type="text"
+                  required
+                  placeholder='Masukkan Nama Habit'
+                  className='bg-gray-200 p-1 rounded-md text-ellipsis'
+                  value={newHabitTitle}
+                  onChange={(e) => setNewHabitTitle(e.target.value)}
+                />
+
+                <input
+                  type="text"
+                  required
+                  placeholder='Masukkan Deskripsi Habit'
+                  className='bg-gray-200 p-1 rounded-md text-ellipsis'
+                  value={newHabitDesc}
+                  onChange={(e) => setNewHabitDesc(e.target.value)}
+                />
+                <div className='text-right mt-2'>
+                </div>
+                <button className='bg-green-400 p-1 px-3 rounded-md text-gray-800 shadow-md' onClick={handleAddHabit}>Tambah</button>
+              </form>
+            </div>
+          </div>
+        }
+
+        <footer className='bg-gray-200 text-center p-4 mt-auto'>
+          <p className=''>© 2025 Habit Tracker Harian by Ihsan. All rights reserved.</p>
+        </footer>
       </div>
-
-
-      {/* Pop-up */}
-      {
-        isVisible &&
-        <div className={`absolute h-screen w-screen inset-0 bg-gray-500/40 flex flex-col justify-center ease-in duration-300`}>
-          <div className='bg-white mx-[10%] md:mx-[30%] xl:mx-[34%] p-4 rounded-md'>
-            <div className='flex justify-between mb-4'>
-              <h1 className='text-xl font-bold'>Tambah Habit</h1>
-              <button className='border border-black/50 w-7 h-7 rounded-full' onClick={() => {setIsVisible(false)}}>X</button>
-            </div>
-            <form action="" className='text-center flex flex-col px-8 gap-3'>
-              <input 
-                type="text" 
-                required 
-                placeholder='Masukkan Nama Habit' 
-                className='bg-gray-200 p-1 rounded-md text-ellipsis'
-                value={newHabitTitle}
-                onChange={(e) => setNewHabitTitle(e.target.value)}
-              />
-
-              <input 
-                type="text" 
-                required 
-                placeholder='Masukkan Deskripsi Habit' 
-                className='bg-gray-200 p-1 rounded-md text-ellipsis'
-                value={newHabitDesc}
-                onChange={(e) => setNewHabitDesc(e.target.value)}
-              />
-              <div className='text-right mt-2'>
-              </div>
-              <button className='bg-green-400 p-1 px-3 rounded-md text-gray-800 shadow-md' onClick={handleAddHabit}>Tambah</button>
-            </form>
-          </div>
-        </div>
-      }
-
-      <footer className='bg-gray-200 text-center p-4 mt-20'>
-        <p className=''>© 2025 Habit Tracker Harian by Ihsan. All rights reserved.</p>
-      </footer>
     </>
   )
 }
